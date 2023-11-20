452acbc82ef08be3ad8e42c984df5024c0f6fe1e
Cecilia <127440950+MCcamposs@users.noreply.github.com>
2023-11-20 17:42:34 +0100

    by:id termiando

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,21 +13,42 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CountryPageComponent implements OnInit{
 
+  public country?: Country;
   // necesito dos cosas, el url C
-  // pdoer navegar
+  // pdoer navegar a la personal y eso nos lo ofrece router de angualr
 
-  constructor( private activatedRoute: ActivatedRoute ){
+  //servicias a importar Activated Router
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router,
+    ){
 
   }
 
 
-
-  // params por si es ub observable, imprimimos en consola los parámetros
+  // params es un observable, imprimimos en consola los parámetros
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe( params => {
-      console.log(params)
-    } )
+    this.activatedRoute.params
+    .pipe(
+      switchMap (({ id }) => this.countriesService.searchCountryByAlphaCode( id ))
+    )
+    .subscribe( ( country ) => {
+
+      if ( !country ){
+        return this.router.navigateByUrl('');
+      }
+      return this.country = country;
+
+      })
+    };
   }
 
 
-}
+
+
+
+// Suscribirse a los cambios en los parámetros de la ruta
+    //Este código se suscribe a los cambios en los parámetros de la ruta.
+    // Cuando hay cambios en los parámetros de la ruta, la función de
+    //devolución de llamada proporcionada se ejecuta.
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from '../interfaces/country';
 
-import { Observable, catchError, of,} from 'rxjs';
+import { Observable, catchError, map, of,} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
@@ -14,6 +14,8 @@ export class CountriesService {
 
   constructor(private http: HttpClient){ } // tenemos que importar el httpclient, en appModule
 
+
+  //FUNCIONES DE BUSCAR POR PAIS, REGIONI Y CAPITAL
   searchCapital( term: string ):Observable<Country[]> {
     const url = `${ this.apiURL }/capital/${ term }`
     return this.http.get<Country[]>(url).pipe(
@@ -35,5 +37,17 @@ export class CountriesService {
     );
   }
 
+  //---------------------------------------------------------
+  //--------------------METODO DE BUSQUEDA-------------------
+  //---------------------------------------------------------
+
+  searchCountryByAlphaCode (code: string):Observable<Country | null>{
+    const url = `${ this.apiURL }/alpha/${ code }`
+    return this.http.get<Country[]>(url).
+    pipe(
+      map(countries => countries.length > 0 ? countries[0]: null),
+      catchError( error => of(null))
+     );
+  }
 
 }
